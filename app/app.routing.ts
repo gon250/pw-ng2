bfdb6ea40fa5724a5941e7610cb08460b85de83b
System Administrator <root@Gonzalos-MacBook-Pro.local>
2016-10-26 17:49:14 +0100

    Adding share components

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -2,7 +2,7 @@ import { NgModule }     from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/index';
-//import { PageNotFoundComponent } from './shared/components/page-not-found/index';
+import { PageNotFoundComponent } from './shared/components/page-not-found/index';
 
 
 @NgModule({
@@ -10,13 +10,12 @@ import { HomeComponent } from './home/index';
         RouterModule.forRoot([
             { path: 'home', component: HomeComponent },
             { path: '', redirectTo: "home", pathMatch: "full" }
-            //,{ path: '**', component: PageNotFoundComponent }
+            ,{ path: '**', component: PageNotFoundComponent }
         ], { useHash: true })
     ],
     exports: [
         RouterModule
     ]
 })
-
 
 export class AppRoutingModule { }
---
+++ app/shared/page-not-found/page-not-found.component.ts
@@ -0,0 +1,13 @@
+
+import { Component } from "@angular/core";
+
+
+@Component({
+    moduleId: module.id,
+    selector: "page-not-found-component",
+    templateUrl: "./page-not-found.component.html",
+    styleUrls: ["./page-not-found.component.css"]
+})
+
+
+export class PageNotFoundComponent { }
