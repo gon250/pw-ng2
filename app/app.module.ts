1da65f23e42e9760f890d3c741e19e6b21a1b891
System Administrator <root@Gonzalos-MacBook-Pro.local>
2016-10-27 21:08:07 +0100

    extracting header

--- app/app.module.ts
+++ app/app.module.ts
@@ -11,6 +11,7 @@ import { PortfolioComponent }    from './portfolio/portfolio.component';
 import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 
 import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   imports: [ 
@@ -23,7 +24,8 @@ import { FooterComponent } from './shared/footer/footer.component';
       HomeComponent,
       PortfolioComponent,
       PageNotFoundComponent,
-      FooterComponent
+      FooterComponent,
+      HeaderComponent
     ],
   bootstrap:    [ AppComponent ]
 })
---
+++ app/shared/header/header.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: '<my-header></my-header>',
+    templateUrl: '../app/shared/header/header.component.html'
+})
+export class HeaderComponent { }
