feca7b9b0334fb1f0aff40a7fb4e173848c6b562
System Administrator <root@Gonzalos-MacBook-Pro.local>
2016-10-30 17:20:40 +0000

    working on blog-header & footer

--- app/blog/blog.component.ts
+++ app/blog/blog.component.ts
@@ -2,7 +2,35 @@ import { Component } from '@angular/core';
 import { PostsComponent } from './posts.component';
 
 @Component({
-    templateUrl: '../app/blog/blog.component.html'
+    templateUrl: '../app/blog/blog.component.html',
+    styles: [
+        'h1 {color:white;}', 
+        `.page-header-blog {
+            height: 100vh;
+            background-position: center center;
+            background-size: cover;
+            margin: 0;
+            padding: 0;
+            border: 0;
+         }
+         .page-header-blog  .carousel .carousel-indicators {
+            bottom: 60px;
+            }
+            .page-header-blog  .container {
+            padding-top: 30vh;
+            color: #FFFFFF;
+            }
+            .page-header-blog  .title {
+            color: #FFFFFF;
+            }
+            .page-header-blog .header-small {
+            height: 65vh;
+            }
+            .page-header-blog .header-small .container {
+            padding-top: 10vh;
+            }
+         `
+        ]
 })
 
 export class BlogComponent {}
