e84a865b1669f30ea4f7f671b12cb633b3d0c79c
System Administrator <root@Gonzalos-MacBook-Pro.local>
2016-10-31 17:47:16 +0000

    Adding style to blog section

--- app/blog/posts.component.ts
+++ app/blog/posts.component.ts
@@ -3,7 +3,17 @@ import { BlogPost } from './blog-post';
 import { BLOGPOSTS } from './mocks';
 @Component({
     selector: 'posts',
-    templateUrl: './app/blog/posts.component.html'
+    templateUrl: './app/blog/posts.component.html',
+    styles: [
+        `.blog-title{
+                color: #F44336;
+            }`, 
+        `.blog-content {
+            color: #999999;
+            font-size: 1.25em;
+            line-height: 1.55em;
+        }`
+    ]
 })
 export class PostsComponent {
     posts: BlogPost[];
